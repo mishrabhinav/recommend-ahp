da1ce5b048a346c08035f12aeb7b305a18136de3
Abhinav Mishra <mishrabhinav96@gmail.com>
2018-03-30 22:23:21 -0700

    Connect to mongod

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
+from os import environ as env
 from flask import Flask
 from flask_restful import Api
+from pymodm import connect
 
-from resources import Index, Retrieve, Select
+from resources import Index, Retrieve, Select, User
 
+connect("mongodb://{}:{}/{}".format(env['MONGO_IP'], env['MONGO_PORT'], env['MONGO_DB']), alias="recommend-ahp")
 
-def main():
-    app = Flask(__name__)
-    api = Api(app)
+app = Flask(__name__)
+api = Api(app)
 
-    api.add_resource(Index, '/', '/api')
-    api.add_resource(Retrieve, '/api/retrieve')
-    api.add_resource(Select, '/api/select')
-
-
-if __name__ != '__main__':
-    main()
+api.add_resource(Index, '/', '/api')
+api.add_resource(Retrieve, '/api/retrieve')
+api.add_resource(Select, '/api/select')
+api.add_resource(User, '/api/user')
