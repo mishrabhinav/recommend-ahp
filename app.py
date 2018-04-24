cb2590d14f34140aae6f42a657ac44da635e545c
Abhinav Mishra <mishrabhinav96@gmail.com>
2018-04-23 20:26:43 -0700

    Add mongo auth fields

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ from flask import Flask
 from flask_restful import Api
 from pymodm import connect
 
-from resources import Index, Retrieve, Select, User
+from resources import Index, Retrieve, Select, User, History
 
-connect("mongodb://{}:{}/{}".format(env['MONGO_IP'], env['MONGO_PORT'], env['MONGO_DB']), alias="recommend-ahp")
+connect("mongodb://{}:{}@{}:{}/{}?authSource=admin".format(env['MONGO_USER'], env['MONGO_PASS'], env['MONGO_IP'],
+                                                           env['MONGO_PORT'],
+                                                           env['MONGO_DB']), alias="recommend-ahp")
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Index, '/', '/api')
 api.add_resource(Retrieve, '/api/retrieve')
+api.add_resource(History, '/api/history')
 api.add_resource(Select, '/api/select')
 api.add_resource(User, '/api/user')
