3edb065865ae49299575dbcaf4a22afa00909666
Abhinav Mishra <mishrabhinav96@gmail.com>
2018-04-23 20:49:23 -0700

    Remove authSource for mlab

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from pymodm import connect
 
 from resources import Index, Retrieve, Select, User, History
 
-connect("mongodb://{}:{}@{}:{}/{}?authSource=admin".format(env['MONGO_USER'], env['MONGO_PASS'], env['MONGO_IP'],
-                                                           env['MONGO_PORT'],
-                                                           env['MONGO_DB']), alias="recommend-ahp")
+connect("mongodb://{}:{}@{}:{}/{}".format(env['MONGO_USER'], env['MONGO_PASS'], env['MONGO_IP'],
+                                          env['MONGO_PORT'],
+                                          env['MONGO_DB']), alias="recommend-ahp")
 
 app = Flask(__name__)
 api = Api(app)
