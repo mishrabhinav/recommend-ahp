edbc9f4fe1c2d5362a89fd4edc13fa6bfd46f75e
Abhinav Mishra <mishrabhinav96@gmail.com>
2018-04-24 15:29:45 -0700

    Add authentication

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ from flask import Flask
 from flask_restful import Api
 from pymodm import connect
 
-from resources import Index, Retrieve, Select, User, History
+import resources
 
 connect(env['MONGODB_URI'], alias='recommend-ahp')
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Index, '/', '/api')
-api.add_resource(Retrieve, '/api/retrieve')
-api.add_resource(History, '/api/history')
-api.add_resource(Select, '/api/select')
-api.add_resource(User, '/api/user')
+api.add_resource(resources.Index, '/', '/api')
+api.add_resource(resources.Retrieve, '/api/retrieve')
+api.add_resource(resources.History, '/api/history')
+api.add_resource(resources.Select, '/api/select')
+api.add_resource(resources.User, '/api/user')
--- resources/index.py
+++ resources/index.py
@@ -1,8 +1,11 @@
 from flask_restful import Resource
 
+from utils.auth import requires_auth
+
 
 class Index(Resource):
     @staticmethod
+    @requires_auth
     def get():
         return {
             'status': 'OK',
